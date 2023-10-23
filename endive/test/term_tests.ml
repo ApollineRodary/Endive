e0dc77d74296d60967fd5cfd0f8be829ebc016a8
Greg Depoire--Ferrer <contact@dfg.re>
2023-10-23 14:40:21 +0200

    Make typing error diagnostics more precise
    Pinpoint the smallest subterm that is considered to be the issue.

--- endive/test/term_tests.ml
+++ endive/test/term_tests.ml
@@ -41,17 +41,17 @@ let%test "normal form: beta reduction under application" =
   normal_form t = t'
 
 let%test "type checking" =
-  let t1 = Lam ((fresh "x", fresh (Var "X")), fresh (Var "x")) in
+  let t1 = fresh (Lam ((fresh "x", fresh (Var "X")), fresh (Var "x"))) in
   let t2 = term_fun (fresh (Var "X")) (fresh (Var "X")) in
-  match ty t1 [] with Some t2' -> alpha_eq t2'.el t2 [] | None -> false
+  match ty t1 [] with Ok t2' -> alpha_eq t2'.el t2 [] | Error _ -> false
 
 let%test "type universe" =
-  let t1 = term_fun (fresh (Var "X")) (fresh (Var "Y")) in
-  let t2 = term_fun (fresh (Univ (fresh 0))) (fresh (Univ (fresh 1))) in
-  let t3 = Univ (fresh 1) in
-  let t4 = Lam ((fresh "X", fresh (Univ (fresh 0))), fresh (Var "X")) in
+  let t1 = fresh (term_fun (fresh (Var "X")) (fresh (Var "Y"))) in
+  let t2 = fresh (term_fun (fresh (Univ (fresh 0))) (fresh (Univ (fresh 1)))) in
+  let t3 = fresh (Univ (fresh 1)) in
+  let t4 = fresh (Lam ((fresh "X", fresh (Univ (fresh 0))), fresh (Var "X"))) in
   univ_level t1 [ ("X", fresh (Univ (fresh 0))); ("Y", fresh (Univ (fresh 0))) ]
-  = Some (fresh 0)
-  && univ_level t2 [] = Some (fresh 2)
-  && univ_level t3 [] = Some (fresh 2)
-  && univ_level t4 [] = None
+  = Ok (fresh 0)
+  && univ_level t2 [] = Ok (fresh 2)
+  && univ_level t3 [] = Ok (fresh 2)
+  && univ_level t4 [] = Error None
