91418df1d75af698d6048a9225421476ed5bfc57
jcauvi <jcauvi@gitlab.aliens-lyon.fr>
2023-10-31 14:59:59 +0100

    Suggestion box with text when hover over hint button

--- endive-gui/Interface/assets/scripts/hide_show_hint_button.js
+++ endive-gui/Interface/assets/scripts/hide_show_hint_button.js
@@ -1,7 +1,17 @@
 function show_hb(){
-   $(".hb").show();
+   $(".hb").fadeIn(400);
 }
 
 function hide_hb(){
-   $(".hb").hide();
+   $(".hb").fadeOut(400);
 }
+
+function in_hb(){
+	$('#suggest_box').fadeIn(200);
+}
+
+function out_hb(){
+	$('#suggest_box').fadeOut(200);
+}
+
+$("#hb_suggest").hover(in_hb,out_hb)
