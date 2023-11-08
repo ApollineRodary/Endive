1904b9c66a0beb4dae8b84917b54b43aaf4495a7
Greg Depoire--Ferrer <contact@dfg.re>
2023-11-08 18:17:52 +0100

    Add Nix package
    Add two Nix package to the flake:
    - one with the main endive executable as well as the LSP server, and
    - another one with the HTML files.

--- flake.nix
+++ flake.nix
@@ -2,14 +2,46 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, gitignore }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
       in
       {
+        packages.endive = pkgs.ocamlPackages.buildDunePackage {
+          pname = "endive";
+          version = "0.1.0";
+
+          src = gitignore.lib.gitignoreSource ./.;
+
+          nativeBuildInputs = with pkgs.ocamlPackages; [
+            menhir
+          ];
+
+          propagatedBuildInputs = with pkgs.ocamlPackages; [
+            lsp
+            menhirLib
+          ];
+
+          duneVersion = "3";
+
+          meta = {
+            description = "A proof assistant for the Integrated Project computer science course at ENS de Lyon";
+            license = lib.licenses.mit;
+            homepage = "https://github.com/ApollineRodary/Endive";
+          };
+        };
+
+        packages.endive-frontend = ./endive-gui/Interface;
+
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             ocaml
