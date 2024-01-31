f126e2f0e6f6ffd477449d7e1efe3a551284baa9
beviu <contact@beviu.com>
2024-01-31 19:03:00 +0100

    Actually use parameters in inductive type definitions

--- endive/src/validate.ml
+++ endive/src/validate.ml
@@ -3,15 +3,31 @@ open Stmt
 open Term
 
 let validate stmts =
-  let rec add_constructors env signature constructors =
+  let rec currify args body =
+    match args with tl :: hd -> fresh (Pi (tl, currify hd body)) | [] -> body
+  in
+  let bind bindings env =
+    let rec aux bindings normal_forms env =
+      match bindings with
+      | [] -> Ok (env, normal_forms)
+      | (x, t) :: rest -> (
+          match ty t env with
+          | Ok _ ->
+              let t' = normal_form t in
+              aux rest ((x, t') :: normal_forms) ((x.el, t') :: env)
+          | Error e -> Error e)
+    in
+    aux bindings [] env
+  in
+  let rec decl_constructors env params constructors constructors_env =
     match constructors with
     | [] -> Ok env
     | (x, t) :: rest -> (
-        match ty t env with
+        match ty t constructors_env with
         | Ok _ ->
-            let t' = normal_form t in
+            let t' = currify params (normal_form t) in
             let env = (x.el, t') :: env in
-            add_constructors env signature rest
+            decl_constructors env params rest constructors_env
         | Error e -> Error e)
   in
   let rec aux stmts env proven defs errs goal =
@@ -33,14 +49,18 @@ let validate stmts =
         | Ok t1 -> aux rest env (t1.el :: proven) defs errs goal
         | Error e -> aux rest env proven defs (e :: errs) goal)
     | Inductive (signature, constructors) :: rest -> (
-        match ty signature.ty env with
-        | Ok t -> (
-            match
-              add_constructors
-                ((signature.name.el, t) :: env)
-                signature constructors
-            with
-            | Ok env -> aux rest env proven defs errs goal
+        match bind signature.params env with
+        | Ok (t_env, params) -> (
+            match ty signature.ty t_env with
+            | Ok _ -> (
+                let t = normal_form signature.ty in
+                let env = (signature.name.el, currify params t) :: env in
+                let constructors_env = (signature.name.el, t) :: t_env in
+                match
+                  decl_constructors env params constructors constructors_env
+                with
+                | Ok env -> aux rest env proven defs errs goal
+                | Error e -> aux rest env proven defs (e :: errs) goal)
             | Error e -> aux rest env proven defs (e :: errs) goal)
         | Error e -> aux rest env proven defs (e :: errs) goal)
     | Lemma ((x, t), stmts) :: rest -> (
