4eb7e6baec0b8ecb18fe6a0a00f24ee8dc985521
jcauvi <jcauvi@gitlab.aliens-lyon.fr>
2024-02-14 16:33:08 +0100

    Small fix + add Tutorial to menu

--- endive-gui/Interface/assets/scripts/hide_show_hint_button.js
+++ endive-gui/Interface/assets/scripts/hide_show_hint_button.js
@@ -1,23 +1,24 @@
 function show_hb() {
-  $(".hb").fadeIn(400);
+  jQuery(".hb").fadeIn(400);
 }
 
 function hide_hb() {
-  $(".hb").fadeOut(400);
+  jQuery(".hb").fadeOut(400);
 }
 
 function in_hb() {
-  $("#suggest_box").fadeIn(200);
+  jQuery("#suggest_box").fadeIn(200);
 }
 
 function out_hb() {
-  $("#suggest_box").fadeOut(200);
+  jQuery("#suggest_box").fadeOut(200);
 }
 
 function set_suggestion(text) {
-  $("#suggestion_text").replaceWith(
+  jQuery("#suggestion_text").replaceWith(
     "<div id=" + '"suggestion_text">' + text + "</div>",
   );
 }
 
-$("#hb_suggest").hover(in_hb, out_hb);
+jQuery("#hb_suggest").hover(in_hb, out_hb);
+
