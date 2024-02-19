d0665968ae48ae1bc06be05bebc0635b36c8cdbf
Greg Depoire--Ferrer <contact@dfg.re>
2024-02-19 11:54:08 +0100

    Remove constructor type

--- endive/src/stmt.ml
+++ endive/src/stmt.ml
@@ -10,12 +10,10 @@ type inductive_sig = {
 }
 [@@deriving compare, sexp]
 
-type constructor = string annotated * term annotated [@@deriving compare, sexp]
-
 type stmt =
   | Def of string annotated * term annotated
   | Exact of term annotated
-  | Inductive of inductive_sig * constructor list
+  | Inductive of inductive_sig * annotated_binding list
   | Lemma of annotated_binding * stmt list
   | Let of annotated_binding
 [@@deriving compare, sexp]
