c6f661112899a3351b23d81b8fb8d9e50ef2cf1a
Greg Depoire--Ferrer <contact@dfg.re>
2024-03-01 13:44:35 +0100

    Add stub crate for the engine
    The engine will be responsible for storing definitions and executing
    statements. It will be used to provide the interactive environment.

---
+++ endive-engine/src/lib.rs
@@ -0,0 +1,12 @@
+//! The Endive engine is the core of Endive. It knows about current definitions, and can execute
+//! statements that correspond to the higher level language. It is the model behind the interactive
+//! environment.
+
+pub struct Engine {
+}
+
+impl Engine {
+    pub fn new() -> Engine {
+        Engine {}
+    }
+}
