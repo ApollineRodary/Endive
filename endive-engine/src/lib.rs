d98e000aa2232e608dcc45a6e62939b602dc5301
Greg Depoire--Ferrer <contact@dfg.re>
2024-03-01 13:54:36 +0100

    Add method to translate high level term to kernel

--- endive-engine/src/lib.rs
+++ endive-engine/src/lib.rs
@@ -2,11 +2,51 @@
 //! statements that correspond to the higher level language. It is the model behind the interactive
 //! environment.
 
-pub struct Engine {
-}
+use endive_kernel::{Binding, Ix};
+use endive_lambda::Tm;
+
+pub struct Engine {}
 
 impl Engine {
     pub fn new() -> Engine {
         Engine {}
     }
+
+    /// Translate a lambda term from the higher level language to the kernel language.
+    fn translate_tm(
+        &self,
+        tm: &Tm,
+        env: &mut Vec<String>,
+    ) -> Result<endive_kernel::Tm, TranslateError> {
+        Ok(match tm {
+            Tm::Id(id) => {
+                let ix = Ix(env
+                    .iter()
+                    .position(|env_id| env_id == id)
+                    .ok_or(TranslateError::UnboundVariable)?);
+                endive_kernel::Tm::Var(ix)
+            }
+            Tm::Abs(binding) => {
+                let bound_ty = self.translate_tm(&binding.bound_ty, env)?;
+
+                env.push(binding.bound_name.clone());
+                let body = self.translate_tm(&binding.body, env);
+                env.pop();
+
+                endive_kernel::Tm::Abs(Box::new(Binding {
+                    bound_ty,
+                    body: body?,
+                }))
+            }
+            Tm::App(n, m) => endive_kernel::Tm::App(
+                Box::new(self.translate_tm(n, env)?),
+                Box::new(self.translate_tm(m, env)?),
+            ),
+            _ => todo!(),
+        })
+    }
+}
+
+enum TranslateError {
+    UnboundVariable,
 }
