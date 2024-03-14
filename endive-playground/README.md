54a6d9a651eab51b2d67c7694f9f930399c35561
Apolline Rodary <apollinerodary@gmail.com>
2024-03-14 10:23:26 +0100

    Endive playground with minimal codegen and proof verification

---
+++ endive-playground/README.md
@@ -0,0 +1,12 @@
+Build the WebAssembly package:
+```sh
+cd crates/endive-wasm
+wasm-pack build --target=web
+```
+
+Start an HTTP server at `http://localhost:1234`:
+```sh
+npm run serve
+```
+
+Currently the playground supports Endive tactics `then` (`so`? we haven't done the arguing yet but that's on my to-do-list), `let` (only supporting type `Prop`), and `suppose`. The only things that can be proven are implications, but a few blocks with no function (OR, AND, lemmas) have been implemented.
---
+++ endive-playground/src/scripts/main.js
@@ -0,0 +1,107 @@
+import { endiveGenerator } from "./generators/endive.js";
+import { verifyTheorems } from "./verify.js";
+
+const updateCodeEvents = new Set([
+  Blockly.Events.BLOCK_CHANGE,
+  Blockly.Events.BLOCK_CREATE,
+  Blockly.Events.BLOCK_DELETE,
+  Blockly.Events.BLOCK_MOVE,
+]);
+
+function updateCode(event) {
+  if (workspace.isDragging()) return;
+  if (!updateCodeEvents.has(event.type)) return;
+
+  const endiveCode = endiveGenerator.workspaceToCode(workspace);
+  document.getElementById("endivecodearea").value = endiveCode;
+  // document.getElementById('latexcodearea').value = latexCode;
+}
+
+const verifyProofs = function (button) {
+  console.log(verifyTheorems(workspace));
+};
+
+var toolbox = {
+  kind: "categoryToolbox",
+  contents: [
+    {
+      kind: "category",
+      name: "Théorèmes",
+      contents: [
+        {
+          kind: "block",
+          type: "theorem",
+        },
+        {
+          kind: "block",
+          type: "lemma",
+        },
+      ],
+    },
+    {
+      kind: "category",
+      name: "Formules",
+      contents: [
+        {
+          kind: "block",
+          type: "proposition_forall",
+        },
+        {
+          kind: "block",
+          type: "proposition_exists",
+        },
+        {
+          kind: "block",
+          type: "proposition_or",
+        },
+        {
+          kind: "block",
+          type: "proposition_and",
+        },
+        {
+          kind: "block",
+          type: "proposition_implies",
+        },
+        {
+          kind: "block",
+          type: "proposition_reference",
+        },
+      ],
+    },
+    {
+      kind: "category",
+      name: "Preuve",
+      contents: [
+        {
+          kind: "block",
+          type: "tactic_let",
+        },
+        {
+          kind: "block",
+          type: "tactic_suppose",
+        },
+        {
+          kind: "block",
+          type: "tactic_then",
+        },
+        {
+          kind: "button",
+          text: "Vérifier",
+          callbackKey: "verifyProofs",
+        },
+      ],
+    },
+  ],
+};
+
+var workspace = Blockly.inject("blocklyDiv", {
+  toolbox: toolbox,
+  scrollbars: false,
+  horizontalLayout: false,
+  toolboxPosition: "start",
+});
+
+endiveGenerator.init(workspace);
+workspace.clear();
+workspace.addChangeListener(updateCode);
+workspace.registerButtonCallback("verifyProofs", verifyProofs);
