b33b8e564a20b0a02679c736e16e32cbd8de9b19
Greg Depoire--Ferrer <contact@dfg.re>
2024-03-16 01:05:37 +0100

    Add inductives to global env before validation
    This will be useful to construct terms that refer to it.

--- crates/endive-kernel/src/global_env.rs
+++ crates/endive-kernel/src/global_env.rs
@@ -1,4 +1,4 @@
-use crate::InductiveTypeFamily;
+use crate::{Error, InductiveTypeFamily};
 
 /// Global environment, which stores inducive type families.
 pub struct GlobalEnv {
@@ -14,8 +14,18 @@ impl GlobalEnv {
         }
     }
 
-    /// Adds an inductive type family to the global environment.
-    pub fn add_inductive(&mut self, family: InductiveTypeFamily) {
+    /// Tries to add an inductive type family to the global environment, and returns its index.
+    ///
+    /// If the family is invalid, it is not added to the environment and an error is returned.
+    pub fn add_inductive(&mut self, family: InductiveTypeFamily) -> Result<usize, Error> {
+        let index = self.inductives.len();
         self.inductives.push(family);
+        match self.inductives[index].validate(self, index) {
+            Ok(()) => Ok(index),
+            Err(e) => {
+                self.inductives.pop();
+                Err(e)
+            }
+        }
     }
 }
