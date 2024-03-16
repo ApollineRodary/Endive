e2e555462f7a330ce925e60c9b2bcca6bb39cabc
Greg Depoire--Ferrer <contact@dfg.re>
2024-03-16 04:06:29 +0100

    Add an iter method to a local context
    This will be used by a later commit.

--- crates/endive-kernel/src/ctx.rs
+++ crates/endive-kernel/src/ctx.rs
@@ -39,11 +39,34 @@ impl Ctx {
     pub(crate) fn push(self: &Rc<Self>, val: Val) -> Rc<Self> {
         Rc::new(Ctx::Cons(val, self.clone()))
     }
+
+    /// Returns an iterator over the values in the local context.
+    pub(crate) fn iter(&self) -> CtxIter {
+        CtxIter {
+            ctx: self,
+            i: Ix(0),
+        }
+    }
 }
 
 impl Default for Ctx {
     fn default() -> Self {
         Ctx::Nil
+    }
+}
+
+pub(crate) struct CtxIter<'a> {
+    ctx: &'a Ctx,
+    i: Ix,
+}
+
+impl<'a> Iterator for CtxIter<'a> {
+    type Item = &'a Val;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let val = self.ctx.get(self.i);
+        self.i.0 += 1;
+        val
     }
 }
 
