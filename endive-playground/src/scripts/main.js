7b55cc93134b2946235a54b18116af3be53a424d
Apolline Rodary <apollinerodary@gmail.com>
2024-03-16 13:36:44 +0100

    Rename a few things and remove unused code

--- endive-playground/src/scripts/main.js
+++ endive-playground/src/scripts/main.js
@@ -1,5 +1,5 @@
 import { endiveGenerator } from "./generators/endive.js";
-import { latexGenerator } from "./generators/latex.js";
+import { htmlGenerator } from "./generators/html.js";
 import { verifyTheorems } from "./verify.js";
 
 const updateCodeEvents = new Set([
@@ -16,10 +16,10 @@ function updateCode(event) {
   if (workspace.isDragging()) return;
   if (!updateCodeEvents.has(event.type)) return;
 
-  const latexCode = latexGenerator.workspaceToCode(workspace);
-  const latexDiv = document.getElementById("latexcodearea");
-  latexDiv.innerHTML = latexCode;
-  MathJax.typesetPromise([latexDiv]);
+  const htmlCode = htmlGenerator.workspaceToCode(workspace);
+  const mathDisplayDiv = document.getElementById("mathDisplayDiv");
+  mathDisplayDiv.innerHTML = htmlCode;
+  MathJax.typesetPromise([mathDisplayDiv]);
 
   //const endiveCode = endiveGenerator.workspaceToCode(workspace);
   //document.getElementById("endivecodearea").value = endiveCode;
@@ -119,22 +119,22 @@ defaultVariableNames.forEach(function (variableName) {
   workspace.createVariable(variableName);
 });
 
-function togglelatex() {
-  var x = document.getElementById("latexcodearea");
+function toggleMathDisplay() {
+  var x = document.getElementById("mathDisplayDiv");
   if (x.style.display === "none") {
     x.style.display = "block";
     document.getElementById("blocklyDiv").style.width = "70%";
-    document.getElementById("togglelatex").classList.add("pressed");
+    document.getElementById("toggleMathDisplay").classList.add("pressed");
     resize();
   } else {
     x.style.display = "none";
     document.getElementById("blocklyDiv").style.width = "100%";
-    document.getElementById("togglelatex").classList.remove("pressed");
+    document.getElementById("toggleMathDisplay").classList.remove("pressed");
     resize();
   }
 }
 
-let button = document.getElementById("togglelatex");
-button.addEventListener("click", togglelatex);
+let button = document.getElementById("toggleMathDisplay");
+button.addEventListener("click", toggleMathDisplay);
 
 endiveGenerator.init(workspace);
